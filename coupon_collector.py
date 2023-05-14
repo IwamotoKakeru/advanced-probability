f724f912ad2e6c1c53efb21258d776992980c23a
IwamotoKakeru <kganpipi@gmail.com>
2023-05-14 17:11:47 +0900

    first commit

---
+++ coupon_collector.py
@@ -0,0 +1,12 @@
+# クーポンコレクタ問題
+class Nodes:
+
+    def __init__(self):
+        self.node_num = 0
+        self.max_num = 0
+
+    def __init__(self, node_num, max_num):
+        self.node_num = node_num
+        self.max_num = max_num
+
+    visit_flag = false
